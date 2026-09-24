eef3dcabfc94189232c8d4b40ff3c19f418e4653
Luan Santos <luanjanz0@gmail.com>
2026-09-24 20:18:02 -0300

    Add main.c to print asterisks in a grid

---
+++ Aula_4_Parte_4/Atividade_3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+
+for (int linha = 1; linha <= 3; linha++) {
+
+    for (int coluna = 1; coluna <= 4; coluna++) {
+        printf("*");
+    }
+
+    printf("\n");
+}
+
+return 0;
+}
