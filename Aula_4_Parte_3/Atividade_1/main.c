d7911c0b473bbdd7009ecd139e47a3eab016326f
Luan Santos <luanjanz0@gmail.com>
2026-09-24 20:03:46 -0300

    Implement for and while loops in main.c
    Added two loops demonstrating 'for' and 'while' constructs in C.

---
+++ Aula_4_Parte_3/Atividade_1/main.c
@@ -0,0 +1,25 @@
+// loop com for
+#include <stdio.h>
+int main(){
+
+for (int i = 1; i<= 5; i++) {
+    printf("%d\n", i);
+}
+
+  return 0;
+}
+
+//loop com while
+#include <stdio.h>
+
+int main(){
+  
+int i = 1;
+
+while (i <=5) {
+    printf("%d\n", i);
+    i++;
+}
+
+    return 0;
+}
