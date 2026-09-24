dc588c7dea993dbf79b5d50c18eb9a36532aec57
Luan Santos <luanjanz0@gmail.com>
2026-09-24 20:19:58 -0300

    Add nested loops for multiplication table

--- Aula_4_Parte_4/Atividade_3/main.c
+++ Aula_4_Parte_4/Atividade_3/main.c
@@ -1,3 +1,5 @@
+//Laços aninhados
+
 #include <stdio.h>
 int main(){
 
@@ -11,4 +13,17 @@ for (int linha = 1; linha <= 3; linha++) {
 }
 
 return 0;
+}
+
+// tabela multiplicação 10x10
+
+#include <stdio.h>
+int main(){
+
+for (int i = 1; i <= 10; i++) {
+    for (int j = 1; j <= 10; j++) {
+        printf("%4d", i * j);
+    }
+    printf("\n");
+    }
 }
