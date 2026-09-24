017afcdc70f1d5560c5c59b77e928e89d0b75b29
Luan Santos <luanjanz0@gmail.com>
2026-09-24 19:13:38 -0300

    Add main.c for calculating average of grades

---
+++ Aula_4_Parte_1/Atividade_4/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    float nota;
+    float soma = 0;
+    int quantidade = 0;
+    
+    printf("Digite uma nota ou -1 para sair: ");
+    scanf("%f", &nota);
+    
+    while (nota != -1) {
+        soma += nota;
+        quantidade++;
+    
+        printf("Digite uma nota ou -1 para sair: ");
+        scanf("%f", &nota);
+    }
+    
+    if (quantidade > 0) {
+        printf("Media = %.2f\n", soma / quantidade);
+    }
+
+
+    return 0;
+}
